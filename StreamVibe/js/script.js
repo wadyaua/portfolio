56837fa6518107988fd116f31913c07aca599dbe
wadyaua <wadyaua2.0@gmail.com>
2025-08-02 22:12:19 +0300

    Add files via upload

---
+++ StreamVibe/js/script.js
@@ -0,0 +1,16 @@
+let documentActions = (e) => {
+    const targetElement = e.target;
+
+    if (targetElement.closest('.menu-icon__button')) {
+        document.documentElement.classList.toggle('open-menu');
+
+        // Заборона прокрутки
+        if (document.documentElement.classList.contains('open-menu')) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = '';
+        }
+    }
+};
+
+document.addEventListener('click', documentActions);
